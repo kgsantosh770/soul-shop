9ac2819c440087c98458f6139eafc43dfc646cea
Santosh <s.kunga.ganeshbabu@accenture.com>
2024-02-11 07:38:04 +0530

    header component included in layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import Header from "./components/Header/Header";
+import { Outlet } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
+        <Header />
+        <Outlet />
     </div>
   );
 }
