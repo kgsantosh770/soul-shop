8d286ff62c31d09720f1fdcf1a82311369a89b9e
Santosh <s.kunga.ganeshbabu@accenture.com>
2024-02-11 21:42:59 +0530

    image component

---
+++ src/components/Image/Image.js
@@ -0,0 +1,12 @@
+import { DESKTOP_MIN_WIDTH } from '../../utils/constants';
+
+const Image = ({ image, alt, title, desktopImage = null, className='' }) => {
+    return (
+        <picture>
+            {desktopImage && <source srcSet={desktopImage} media={`(min-width: ${DESKTOP_MIN_WIDTH}px)`} />}
+            <img className={className} alt={alt} title={title} src={image} />
+        </picture>
+    )
+}
+
+export default Image
