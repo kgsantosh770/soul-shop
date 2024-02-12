49508f9d87861ed9f086da3d916e7098a7380065
Santosh <s.kunga.ganeshbabu@accenture.com>
2024-02-12 11:14:13 +0530

    ai characters data

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,115 @@
-const DESKTOP_MIN_WIDTH = 768;
+import Person1Image from '../assets/images/illustrations/person-1.png';
+import Person1NameImage from '../assets/images/illustrations/person-1-withbgname.png';
+import Person2Image from '../assets/images/illustrations/person-2.png';
+import Person2NameImage from '../assets/images/illustrations/person-2-withbgname.png';
+import Person3Image from '../assets/images/illustrations/person-3.png';
+import Person3NameImage from '../assets/images/illustrations/person-3-withbgname.png';
+import Person4Image from '../assets/images/illustrations/person-4.png';
+import Person4NameImage from '../assets/images/illustrations/person-4-withbgname.png';
+import Person5Image from '../assets/images/illustrations/person-5.png';
+import Person5NameImage from '../assets/images/illustrations/person-5-withbgname.png';
+import Person6Image from '../assets/images/illustrations/person-6.png';
+import Person6NameImage from '../assets/images/illustrations/person-6-withbgname.png';
 
-export { DESKTOP_MIN_WIDTH }
+const DESKTOP_MIN_WIDTH = 768;
+const TOTAL_RATING = 5;
+const characters = [
+    {
+        id: 1,
+        image: Person1Image,
+        imageWithName: Person1NameImage,
+        name: 'Allie',
+        price: 235.99,
+        rating: 3,
+    },
+    {
+        id: 2,
+        image: Person2Image,
+        imageWithName: Person2NameImage,
+        name: 'Ryan',
+        price: 235.99,
+        rating: 3,
+    },
+    {
+        id: 3,
+        image: Person3Image,
+        imageWithName: Person3NameImage,
+        name: 'Jossie',
+        price: 235.99,
+        rating: 3,
+    },
+    {
+        id: 4,
+        image: Person4Image,
+        imageWithName: Person4NameImage,
+        name: 'Josnua',
+        price: 235.99,
+        rating: 3,
+    },
+    {
+        id: 5,
+        image: Person5Image,
+        imageWithName: Person5NameImage,
+        name: 'Nichole',
+        price: 235.99,
+        rating: 3,
+    },
+    {
+        id: 6,
+        image: Person6Image,
+        imageWithName: Person6NameImage,
+        name: 'Nichole',
+        price: 235.99,
+        rating: 3,
+    },
+    {
+        id: 7,
+        image: Person1Image,
+        imageWithName: Person1NameImage,
+        name: 'Allie',
+        price: 235.99,
+        rating: 3,
+    },
+    {
+        id: 8,
+        image: Person2Image,
+        imageWithName: Person2NameImage,
+        name: 'Ryan',
+        price: 235.99,
+        rating: 3,
+    },
+    {
+        id: 9,
+        image: Person3Image,
+        imageWithName: Person3NameImage,
+        name: 'Jossie',
+        price: 235.99,
+        rating: 3,
+    },
+    {
+        id: 10,
+        image: Person4Image,
+        imageWithName: Person4NameImage,
+        name: 'Josnua',
+        price: 235.99,
+        rating: 3,
+    },
+    {
+        id: 11,
+        image: Person5Image,
+        imageWithName: Person5NameImage,
+        name: 'Nichole',
+        price: 235.99,
+        rating: 3,
+    },
+    {
+        id: 12,
+        image: Person6Image,
+        imageWithName: Person6NameImage,
+        name: 'Nichole',
+        price: 235.99,
+        rating: 3,
+    },
+]
+
+export { DESKTOP_MIN_WIDTH, TOTAL_RATING, characters}
