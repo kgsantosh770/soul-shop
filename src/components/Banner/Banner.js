d35bee3ccff578ea8e86957fd210084ea74c727d
Santosh <s.kunga.ganeshbabu@accenture.com>
2024-02-14 05:53:46 +0530

    removed style prop

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,15 +1,15 @@
 import './Banner.css';
 
-const Banner = ({ beforeHighlightText, highlightText, afterHighlightText, children }) => {
+const Banner = ({ beforeHighlightText, highlightText, afterHighlightText, children, style }) => {
     return (
-        <>
+        <div style={style ? style : {}}>
             <div className="banner-text">
                 <p>{beforeHighlightText}</p>
                 <p className="highlight">{highlightText}</p>
                 <p>{afterHighlightText}</p>
             </div>
             {children && children}
-        </>
+        </div>
 
     )
 }
