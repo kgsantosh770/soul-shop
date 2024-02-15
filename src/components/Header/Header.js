c0537f8553e2e8b822d9de9cd34ae73a91ea21fd
Santosh <s.kunga.ganeshbabu@accenture.com>
2024-02-15 08:36:37 +0530

    header and footer ui fixes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import { DESKTOP_MIN_WIDTH } from '../../utils/constants';
 import LogoDesktop from '../../assets/images/logos/logo-desktop.svg';
 import LogoMobile from '../../assets/images/logos/logo-mobile.svg';
@@ -28,7 +28,7 @@ const Header = () => {
           (link, index) =>
             <li key={index}>
               <NavLink
-                exact="true"
+                end
                 className={({ isActive, isPending }) =>
                   isActive
                     ? "active"
@@ -45,14 +45,24 @@ const Header = () => {
     </ul>
   )
 
+  const toggleSideMenu = () => {
+    if (typeof window != 'undefined' && window.document) {
+      if (document.body.style.overflow === 'hidden')
+        document.body.style.overflow = 'auto';
+      else
+        document.body.style.overflow = 'hidden';
+    }
+    setisMobileMenuOpen(prev => !prev);
+  }
+
   return (
     <header>
-      <button>
+      <Link to={'/'}>
         <picture>
           <source srcSet={LogoDesktop} media={`min-width: ${DESKTOP_MIN_WIDTH}px`} />
           <img alt='logo' title="Soul Shop" src={LogoMobile} />
         </picture>
-      </button>
+      </Link>
 
       <div className='only-desktop'>
         {navLinkElements}
@@ -64,25 +74,27 @@ const Header = () => {
             <img className='icon' alt='Cart' title='Cart' src={CartIcon} />
           </IconBadge>
         </button>
-        <button className='only-mobile' onClick={() => { setisMobileMenuOpen(true) }}>
+        <button className='only-mobile' onClick={toggleSideMenu}>
           <img className='icon icon-large' alt='Menu' title='Menu' src={MenuIcon} />
         </button>
       </div>
 
       <div className={`only-mobile side-nav ${isMobileMenuOpen ? 'open' : ''}`}>
-        <button className='close-btn' onClick={() => { setisMobileMenuOpen(false) }}>
-          <img className='icon icon-large' alt='close' title='close' src={CloseIcon} />
-        </button>
-        <img alt='logo' title='logo' src={LogoMobileLarge} />
-        {navLinkElements}
-        <SocialBox className='divider-top' />
-        <div className='nav-footer'>
-          <p>Download</p>
-          <img className='app-store' alt='playstore' title='playstore' src={PlayStoreImage} />
-          <img className='app-store' alt='appstore' title='appstore' src={AppStoreImage} />
-          <p>Lorem ipsum dolor</p>
-          <p>sit amet, consectetur.</p>
-          <p className='trademark'>© Soul-Shop,inc 2024</p>
+        <div className='inner-content'>
+          <button className='close-btn' onClick={toggleSideMenu}>
+            <img className='icon icon-large' alt='close' title='close' src={CloseIcon} />
+          </button>
+          <img alt='logo' title='logo' src={LogoMobileLarge} />
+          {navLinkElements}
+          <SocialBox className='divider-top' />
+          <div className='nav-footer'>
+            <p>Download</p>
+            <img className='app-store' alt='playstore' title='playstore' src={PlayStoreImage} />
+            <img className='app-store' alt='appstore' title='appstore' src={AppStoreImage} />
+            <p>Lorem ipsum dolor</p>
+            <p>sit amet, consectetur.</p>
+            <p className='trademark'>© Soul-Shop,inc 2024</p>
+          </div>
         </div>
       </div>
     </header>
