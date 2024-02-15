ada40dad7cbe1f57ed212d7199e039e845822ccc
Santosh <s.kunga.ganeshbabu@accenture.com>
2024-02-16 00:37:06 +0530

    payment option page

---
+++ src/pages/PaymentOptions/PaymentOptions.js
@@ -0,0 +1,66 @@
+import LockIcon from '../../assets/images/icons/lock.svg';
+import { PAYMENT_OPTIONS as paymentOptions, TAX } from '../../utils/constants';
+import { NavigationButtons } from '../Checkout/Checkout';
+import './PaymentOptions.css';
+
+const PaymentOptions = () => {
+    const subtotal = 18900.99;
+    const OptionCard = ({ option, checked }) => (
+        <div className="option-card box-border">
+            <input
+                type="radio"
+                id={option.name}
+                name="option"
+                value={option.name}
+                defaultChecked={checked}
+            />
+            <div>
+                <label htmlFor={option.name}>{option.name}</label>
+                <p className='desc'>{option.desc}</p>
+            </div>
+            <div className="images">
+                {
+                    option.acceptedMethods.map((acceptedMethod, index) => (
+                        <img
+                            key={index}
+                            alt={acceptedMethod.name}
+                            title={acceptedMethod.name}
+                            src={acceptedMethod.image}
+                        />
+                    ))
+                }
+            </div>
+        </div>
+    )
+    return (
+        <div className="pay-options-page">
+            <p className="title">Select payment option</p>
+            <div>
+                <div className="key-value box-border">
+                    <div>
+                        <p className='title'>Total</p>
+                        <p className='subtitle'>Including ₹{TAX} in taxes</p>
+                    </div>
+                    <p className='value'>₹ {subtotal + 70}</p>
+                </div>
+                {
+                    paymentOptions.map(
+                        (option, index) =>
+                            <OptionCard
+                                key={index}
+                                option={option}
+                                checked={index === 0}
+                            />
+                    )
+                }
+            </div>
+            <div className='security'>
+                <img alt='secured' title='secured' src={LockIcon} />
+                <span>We protect your payment information using encryption to provide bank-level security.</span>
+            </div>
+            <NavigationButtons backRoute={'/checkout'} nextRoute={'/payment'} />
+        </div>
+    )
+}
+
+export default PaymentOptions
