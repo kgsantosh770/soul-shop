3f40b3e3c13205f8b6cedec91296a2b0b5f2c23c
Santosh <s.kunga.ganeshbabu@accenture.com>
2024-02-15 07:12:34 +0530

    extra data added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,14 +1,20 @@
 import Person1Image from '../assets/images/illustrations/person-1.png';
+import Person1BgImage from '../assets/images/illustrations/person-1-withbg.png';
 import Person1NameImage from '../assets/images/illustrations/person-1-withbgname.png';
 import Person2Image from '../assets/images/illustrations/person-2.png';
+import Person2BgImage from '../assets/images/illustrations/person-2-withbg.png';
 import Person2NameImage from '../assets/images/illustrations/person-2-withbgname.png';
 import Person3Image from '../assets/images/illustrations/person-3.png';
+import Person3BgImage from '../assets/images/illustrations/person-3-withbg.png';
 import Person3NameImage from '../assets/images/illustrations/person-3-withbgname.png';
 import Person4Image from '../assets/images/illustrations/person-4.png';
+import Person4BgImage from '../assets/images/illustrations/person-4-withbg.png';
 import Person4NameImage from '../assets/images/illustrations/person-4-withbgname.png';
 import Person5Image from '../assets/images/illustrations/person-5.png';
+import Person5BgImage from '../assets/images/illustrations/person-5-withbg.png';
 import Person5NameImage from '../assets/images/illustrations/person-5-withbgname.png';
 import Person6Image from '../assets/images/illustrations/person-6.png';
+import Person6BgImage from '../assets/images/illustrations/person-6-withbg.png';
 import Person6NameImage from '../assets/images/illustrations/person-6-withbgname.png';
 import Brand1Image from '../assets/images/illustrations/brand-1.png';
 import Brand2Image from '../assets/images/illustrations/brand-2.png';
@@ -23,109 +29,157 @@ const characters = [
         id: 1,
         image: Person1Image,
         imageWithName: Person1NameImage,
+        imageWithBg: Person1BgImage,
         name: 'Allie',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         price: 235.99,
         rating: 3,
+        period: 'month',
+        features: ['Financial Expert', 'Sports Person', 'Psychologist'],
         tags: ['popular','mostview']
     },
     {
         id: 2,
         image: Person2Image,
         imageWithName: Person2NameImage,
+        imageWithBg: Person2BgImage,
         name: 'Ryan',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         price: 235.99,
         rating: 3,
+        period: 'month',
+        features: ['Financial Expert', 'Sports Person', 'Psychologist'],
         tags: ['popular']
     },
     {
         id: 3,
         image: Person3Image,
         imageWithName: Person3NameImage,
+        imageWithBg: Person3BgImage,
         name: 'Jossie',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         price: 235.99,
         rating: 3,
+        period: 'month',
+        features: ['Financial Expert', 'Sports Person', 'Psychologist'],
         tags: ['mostview']
     },
     {
         id: 4,
         image: Person4Image,
         imageWithName: Person4NameImage,
+        imageWithBg: Person4BgImage,
         name: 'Josnua',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         price: 235.99,
         rating: 3,
+        period: 'month',
+        features: ['Financial Expert', 'Sports Person', 'Psychologist'],
         tags: ['popular']
     },
     {
         id: 5,
         image: Person5Image,
         imageWithName: Person5NameImage,
+        imageWithBg: Person5BgImage,
         name: 'Nichole',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         price: 235.99,
         rating: 3,
+        period: 'month',
+        features: ['Financial Expert', 'Sports Person', 'Psychologist'],
         tags: ['mostview']
     },
     {
         id: 6,
         image: Person6Image,
         imageWithName: Person6NameImage,
+        imageWithBg: Person6BgImage,
         name: 'Nichole',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         price: 235.99,
         rating: 3,
+        period: 'month',
+        features: ['Financial Expert', 'Sports Person', 'Psychologist'],
         tags: ['mostview']
     },
     {
         id: 7,
         image: Person1Image,
         imageWithName: Person1NameImage,
+        imageWithBg: Person1BgImage,
         name: 'Allie',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         price: 235.99,
         rating: 3,
+        period: 'month',
+        features: ['Financial Expert', 'Sports Person', 'Psychologist'],
         tags: ['popular','mostview']
     },
     {
         id: 8,
         image: Person2Image,
         imageWithName: Person2NameImage,
+        imageWithBg: Person2BgImage,
         name: 'Ryan',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         price: 235.99,
         rating: 3,
-        tags: ['popular','mostview']
+        period: 'month',
+        features: ['Financial Expert', 'Sports Person', 'Psychologist'],
+        tags: ['popular']
     },
     {
         id: 9,
         image: Person3Image,
         imageWithName: Person3NameImage,
+        imageWithBg: Person3BgImage,
         name: 'Jossie',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         price: 235.99,
         rating: 3,
-        tags: ['popular']
+        period: 'month',
+        features: ['Financial Expert', 'Sports Person', 'Psychologist'],
+        tags: ['mostview']
     },
     {
         id: 10,
         image: Person4Image,
         imageWithName: Person4NameImage,
+        imageWithBg: Person4BgImage,
         name: 'Josnua',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         price: 235.99,
         rating: 3,
-        tags: ['popular', 'mostview']
+        period: 'month',
+        features: ['Financial Expert', 'Sports Person', 'Psychologist'],
+        tags: ['popular']
     },
     {
         id: 11,
         image: Person5Image,
         imageWithName: Person5NameImage,
+        imageWithBg: Person5BgImage,
         name: 'Nichole',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         price: 235.99,
         rating: 3,
+        period: 'month',
+        features: ['Financial Expert', 'Sports Person', 'Psychologist'],
         tags: ['mostview']
     },
     {
         id: 12,
         image: Person6Image,
         imageWithName: Person6NameImage,
+        imageWithBg: Person6BgImage,
         name: 'Nichole',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         price: 235.99,
         rating: 3,
-        tags: ['popular']
+        period: 'month',
+        features: ['Financial Expert', 'Sports Person', 'Psychologist'],
+        tags: ['mostview']
     },
 ]
 
