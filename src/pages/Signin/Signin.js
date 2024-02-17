6bdf34a5a0c1d546f1645e43c3fb6dde65717530
Santosh <s.kunga.ganeshbabu@accenture.com>
2024-02-18 00:04:55 +0530

    link to create account added

--- src/pages/Signin/Signin.js
+++ src/pages/Signin/Signin.js
@@ -1,5 +1,6 @@
 import Form from "../../components/Form/Form";
 import Input from "../../components/Input/Input";
+import { Link } from "react-router-dom";
 
 const Signin = () => {
   return (
@@ -8,6 +9,10 @@ const Signin = () => {
             <Input label='Email' id='email' type='email' placeholder='santosh@gmail.com' />
             <Input label='Password' id='password' type='password' placeholder='Enter your password'/>
             <Input type='submit' id='signinSubmit' value="Signin" />
+            <p className="linker">
+                    Do not have an account ? &nbsp;
+                    <Link to='/register'>Create Account</Link>
+                </p>
         </Form>
     </div>
   )
