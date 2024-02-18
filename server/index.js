3274874e402447a2479ab0db4a7fa2f0701172df
Santosh <s.kunga.ganeshbabu@accenture.com>
2024-02-18 11:55:45 +0530

    server created with express and user authentication implemented.

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import mongoose from "mongoose";
+import UserRouter from "./user/UserRouter.js";
+import "dotenv/config";
+
+const env = process.env;
+const app = express();
+
+app.use(express.json());
+app.use('/api',UserRouter);
+
+app.listen(5000, () => {
+    console.log('Server connect to localhost 5000 successfully.');
+})
+
+app.get('/', (req, res) => {
+    res.json('Hello server');
+})
+
+// Connect to Mongodb
+mongoose.connect(env.MYDB_CONNECTION_STRING)
+    .then(() => {
+        console.log("Mongodb connection successfull.");
+    })
+    .catch((err) => {
+        console.log("Mongodb connection failed: ", err);
+    })
+
+
---
+++ server/user/UserRouter.js
@@ -0,0 +1,39 @@
+import express from "express";
+import User from "./UserSchema.js";
+import bcryptjs from "bcryptjs";
+
+const UserRouter = express.Router();
+
+UserRouter.post('/register', async (req, res) => {
+    const {name, email, password} = req.body;
+    try {
+        const emailExist = await User.findOne({email: email});
+        if(emailExist) throw Error('Email already exist');
+        const salt = bcryptjs.genSaltSync(10);
+        const hashedPassword = bcryptjs.hashSync(password, salt);
+        const user = new User({
+            name: name,
+            email: email,
+            password: hashedPassword,
+        })
+        await user.save();
+        res.status(200).json("Account created successfully.");
+    } catch (error) {
+        res.status(400).json(error.message);
+    }
+})
+
+UserRouter.post('/login',async (req,res)=>{
+    const {email, password} = req.body;
+    try {
+        const user = await User.findOne({email});
+        if(!user) throw Error("Email does not exist.");
+        const passwordMatches = bcryptjs.compareSync(password, user.password);
+        if(!passwordMatches) throw Error("Invalid password");
+        res.status(200).json("Login Success.");
+    } catch (error) {
+        res.status(400).json(error.message);
+    }
+})
+
+export default UserRouter;
