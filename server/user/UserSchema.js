283500fd48f758068b3b41484b56d5af763c4010
Santosh <s.kunga.ganeshbabu@accenture.com>
2024-02-18 13:53:38 +0530

    included timestamp in schema

--- server/user/UserSchema.js
+++ server/user/UserSchema.js
@@ -13,7 +13,7 @@ const schema = new mongoose.Schema({
         type: String,
         required: true,
     },
-})
+}, { timestamps: true })
 
 const User = mongoose.model('User', schema);
 export default User;
