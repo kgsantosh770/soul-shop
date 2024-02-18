ddcf60e5ccab002d92b228c2f02e2982173e0e00
Santosh <s.kunga.ganeshbabu@accenture.com>
2024-02-18 13:53:51 +0530

    cors issue resolved

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import UserRouter from "./user/UserRouter.js";
+import cors from "cors";
 import "dotenv/config";
 
 const env = process.env;
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use('/api',UserRouter);
 
