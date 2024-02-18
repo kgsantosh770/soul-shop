77b8141250d0e557c12166b992aa446a05eec782
Santosh <s.kunga.ganeshbabu@accenture.com>
2024-02-18 13:54:53 +0530

    form action defined

--- client/src/pages/Signin/Signin.js
+++ client/src/pages/Signin/Signin.js
@@ -3,11 +3,12 @@ import Input from "../../components/Input/Input";
 import { Link } from "react-router-dom";
 
 const Signin = () => {
+  const endpoint = `${process.env.REACT_APP_API_BASE_URL}/api/login`;
   return (
     <div className="signin-form">
-        <Form>
-            <Input label='Email' id='email' type='email' placeholder='santosh@gmail.com' />
-            <Input label='Password' id='password' type='password' placeholder='Enter your password'/>
+        <Form actionEndpoint={endpoint}>
+            <Input label='Email' id='email' type='email' placeholder='santosh@gmail.com' required/>
+            <Input label='Password' id='password' type='password' placeholder='Enter your password' required/>
             <Input type='submit' id='signinSubmit' value="Signin" />
             <p className="linker">
                     Do not have an account ? &nbsp;
