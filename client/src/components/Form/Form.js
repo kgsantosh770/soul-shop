7d21677176003abc9cf96de4475344d9488e7ba6
Santosh <s.kunga.ganeshbabu@accenture.com>
2024-02-18 13:54:19 +0530

    used useForm hook

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,13 +1,24 @@
 import './Form.css';
+import useForm from '../../utils/hooks/useForm';
 
-const Form = ({ children }) => {
+const Form = ({ children, actionEndpoint, additionalData }) => {
+    const { handleSubmit } = useForm({additionalData});
     return (
         <div className='custom-form-container'>
-            <form className="custom-form">
+            <form
+                method='POST'
+                action={actionEndpoint}
+                onSubmit={handleSubmit}
+                className="custom-form">
                 {children}
             </form>
         </div>
     )
 }
+
+Form.defaultProps = {
+    additionalData: {}
+}
+
 
 export default Form
