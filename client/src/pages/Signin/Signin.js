ef9414973f0ca22d38bcad863455fd5522857061
Santosh <s.kunga.ganeshbabu@accenture.com>
2024-02-19 12:12:10 +0530

    handled form success scenario

--- client/src/pages/Signin/Signin.js
+++ client/src/pages/Signin/Signin.js
@@ -5,26 +5,26 @@ import useForm from '../../utils/hooks/useForm';
 import { Link, useNavigate } from "react-router-dom";
 
 const Signin = () => {
-  const endpoint = `${process.env.REACT_APP_API_BASE_URL}/api/login`;
+  const endpoint = `${process.env.REACT_APP_API_BASE_URL}/login`;
   const { handleSubmit, result } = useForm();
   const navigate = useNavigate();
 
-  const handleSuccess = () => {
-    const response = result.response;
-    const token = response.headers.get('auth');
-    if (token) localStorage.setItem('auth', token);
-    navigate(-1);
-  }
-
   useEffect(() => {
-    if (result.message)
+    const handleSuccess = () => {
+      const response = result.response;
+      const token = response.headers.get('auth');
+      if (token) localStorage.setItem('auth', token);
+      navigate(-1);
+    }
+
+    if (result.response?.status === 200)
       handleSuccess();
-  }, [result])
+  }, [result, navigate])
 
 
   return (
     <div className="signin-form">
-      <Form actionEndpoint={endpoint} handleSubmit={handleSubmit}>
+      <Form action={endpoint} onSubmit={handleSubmit}>
         <Input label='Email' id='email' type='email' placeholder='santosh@gmail.com' required />
         <Input label='Password' id='password' type='password' placeholder='Enter your password' required />
         <Input type='submit' id='signinSubmit' value="Login" />
