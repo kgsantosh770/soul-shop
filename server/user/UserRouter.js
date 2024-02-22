eef20688b66044edc329878396775b6be1d296f1
Santosh <s.kunga.ganeshbabu@accenture.com>
2024-02-22 23:13:39 +0530

    verify and getuser routes added and respond with user in login route

--- server/user/UserRouter.js
+++ server/user/UserRouter.js
@@ -33,12 +33,38 @@ UserRouter.post('/login',async (req,res)=>{
         if(!user) throw Error("Email does not exist.");
         const passwordMatches = bcryptjs.compareSync(password, user.password);
         if(!passwordMatches) throw Error("Invalid password");
+        delete user.password;
         const userToken = jwt.sign({email: user.email}, env.JWT_ENCODE_STRING);
         res.header('auth',userToken);
         res.header('Access-Control-Expose-Headers', 'auth');
-        res.status(200).json({message: "Login Success."});
+        res.status(200).json({data: user,message: "Login Success."});
     } catch (error) {
         res.status(400).json({error: error.message});
+    }
+})
+
+UserRouter.post('/verifyToken', async(req,res)=>{
+    const {token} = req.body;
+    try {
+        const userData = jwt.verify(token,env.JWT_ENCODE_STRING);
+        if(!userData.email) res.status(401).json({error: "Unauthorized"});
+        res.status(200).json({message: "Authorized"});
+    } catch (error) {
+        res.status(400).json({error: "Something went wront. Please try later."});
+    }
+})
+
+UserRouter.post('/getUser', async(req, res)=>{
+    const {token} = req.body;
+    try {
+        const isValidToken = jwt.verify(token,env.JWT_ENCODE_STRING);
+        if(!isValidToken) res.status(401).json({error: "Unauthorized"});
+        const tokenData = jwt.decode(token);
+        const user = await User.findOne({email: tokenData.email});
+        if(!user) res.status(404).json({error: "Email does not exist."});
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(400).json({error: "Something went wront. Please try later."});
     }
 })
 
