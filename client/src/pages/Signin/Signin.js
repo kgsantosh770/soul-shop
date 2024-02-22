23ef9f9a2250df265e98ddd7bb66c053f6a9645a
Santosh <s.kunga.ganeshbabu@accenture.com>
2024-02-22 23:09:50 +0530

    when logged in store timestamp in local and user in redux state

--- client/src/pages/Signin/Signin.js
+++ client/src/pages/Signin/Signin.js
@@ -3,17 +3,24 @@ import Form from "../../components/Form/Form";
 import Input from "../../components/Input/Input";
 import useForm from '../../utils/hooks/useForm';
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addUser } from "../../redux/userSlice";
 
 const Signin = () => {
   const endpoint = `${process.env.REACT_APP_API_BASE_URL}/login`;
   const { handleSubmit, result } = useForm();
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const handleSuccess = () => {
       const response = result.response;
       const token = response.headers.get('auth');
-      if (token) localStorage.setItem('auth', token);
+      if (token) {
+        localStorage.setItem('auth', token);
+        localStorage.setItem('loggedin_at', new Date());
+        if(result.data) dispatch(addUser(result.data));
+      }
       navigate(-1);
     }
 
