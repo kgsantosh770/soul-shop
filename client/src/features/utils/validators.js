a378dbf8446763c4e0b9ed4de7270de081f3c667
Santosh <s.kunga.ganeshbabu@accenture.com>
2024-04-01 01:31:47 +0530

    email regex string change

--- client/src/features/utils/validators.js
+++ client/src/features/utils/validators.js
@@ -1,5 +1,5 @@
 const isValidEmail = (email) => {
-    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    var validRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     if (validRegex.test(email))
         return true;
     return false;
