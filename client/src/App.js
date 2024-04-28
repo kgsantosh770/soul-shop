923de1bbcb7743f2f9a486591e26f08aa1830b41
kgsantosh770 <santhoshkrishna770@gmail.com>
2024-04-28 19:23:59 +0530

    header and footer full width

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import { Outlet, ScrollRestoration } from "react-router-dom";
 import toast, { Toaster } from "react-hot-toast";
-import './App.css';
+import "./App.css";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { addUser } from "./redux/userSlice";
@@ -11,12 +11,12 @@ import useFetch from "./features/hooks/useFetch";
 
 function App() {
   const dispatch = useDispatch();
-  const { data: userData, loading, error } = useFetch(userApi.getUser())
+  const { data: userData, loading, error } = useFetch(userApi.getUser());
 
   useEffect(() => {
-    if (userData) dispatch(addUser(userData))
+    if (userData) dispatch(addUser(userData));
     if (error) toast.error(error);
-  }, [userData, dispatch, error])
+  }, [userData, dispatch, error]);
 
   return (
     <div className="app">
@@ -24,19 +24,21 @@ function App() {
       <Toaster
         toastOptions={{
           style: {
-            fontFamily: 'var(--font-family-semibold)',
-          }
+            fontFamily: "var(--font-family-semibold)",
+          },
         }}
       />
-      {
-        loading
-          ? <p>Loading...</p>
-          : <>
-            <Header />
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <Header />
+          <main>
             <Outlet />
-            <Footer />
-          </>
-      }
+          </main>
+          <Footer />
+        </>
+      )}
     </div>
   );
 }
